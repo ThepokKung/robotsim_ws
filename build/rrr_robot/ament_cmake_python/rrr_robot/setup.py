f93b052dc0b4b42f9dc9abf5bfee6932553c6676
waritt <waritt10@gmail.com>
2024-10-28 20:19:04 +0700

    rm

--- build/rrr_robot/ament_cmake_python/rrr_robot/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='rrr_robot',
-    version='0.0.0',
-    packages=find_packages(
-        include=('rrr_robot', 'rrr_robot.*')),
-)
--- build/rrr_robot_interfaces/rrr_robot_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/waritt/robotsim_ws/build/rrr_robot_interfaces/rrr_robot_interfaces__py
-# Build directory: /home/waritt/robotsim_ws/build/rrr_robot_interfaces/rrr_robot_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
